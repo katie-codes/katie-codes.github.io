e4ba476cf71adc4eb3ff38d8217a187e1a93e2f9
katie-codes <katiepattersondesign@gmail.com>
2020-07-17 11:04:38 +1000

    Portfolio update
    added footer to all pages, updated header

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    // start
+
+
+<button onclick="myFunction()">Click Me!</button>
+
+<script>
+function myFunction() {
+  var x = document.getElementById("demo");
+  x.style.fontSize = "25px";
+  x.style.color = "red";
+}
+</script>
