c2b762196925b05b54b34ebfb8c13defa669d73e
katie-codes <katiepattersondesign@gmail.com>
2020-07-24 17:55:52 +1000

    updated footer and added proj 1 images

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ window.onload = function() {
     // start
 
 
-<button onclick="myFunction()">Click Me!</button>
-
 <script>
 function myFunction() {
-  var x = document.getElementById("demo");
-  x.style.fontSize = "25px";
-  x.style.color = "red";
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 </script>
+
+
